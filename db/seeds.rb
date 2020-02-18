014c8b844ca178c834ba03163f4ff1cc2590f8ce
Radmo92i <oklm92122@gmail.com>
2020-02-18 21:06:09 +0100

    easy easy e

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+Attendance.destroy_all
+User.destroy_all
+
+10.times do
+   user = User.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, description: Faker::TvShows::GameOfThrones.quote, email: Faker::Internet.email)
+   event = Event.create!(title: Faker::Music::Opera.verdi, description: Faker::Lorem.sentence(word_count: 20), price: Faker::Number.between(from: 1, to: 1000), start_date: Faker::Time.forward(days: 5,  period: :evening, format: :long), location: Faker::Nation.capital_city, duration: Faker::Number.between(from: 1, to: 24))
+   #attendance = Attendance.create!(stripe_customer_id: Faker::IDNumber.valid)
+  end
